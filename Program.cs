4d29c8e4948f91a4433cbecdc7dc353e33c12367
kevinator47 <128663855+kevinator47@users.noreply.github.com>
2023-10-09 20:10:45 -0400

    Version Final
    Siuuu, me voy de fiesta  0>0

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+namespace HULK ;
+using System ;
+using System.Diagnostics ;
+
+/* Punto de Entrada del Programa
+Carga las funciones predefinidas
+Recibe el string ingresado por el usuario que representa la linea de codigo HULK.
+Pasa dicha linea como argumento al Parser(este a su vez llama al Lexer) para convertir el string en expresiones del lenguaje HULK
+Si no hubo ningun problema con la gramatica o la semantica ejecuta el programa
+Si no hay ningun error capturable durante el tiempo de ejecucion(hay algunos que parten el programa para que no se salga de control) imprime el resultado
+Estos pasos se repiten hasta que el usuario ingrese una linea vacia
+
+Tiene colorcitos(verde para el programa normalmente y rojo para mostrar los errores)
+
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        FunctionPool.LoadPredefinedFunctions();
+        var crono = new Stopwatch();
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green ;
+            CompilatorTools.Bugs = new List<string>();
+
+            Console.Write("> ") ;
+            string line = Console.ReadLine();
+            crono.Start();
+
+            if(string.IsNullOrWhiteSpace(line))
+                return ;
+
+            if(line == "#clear")
+            {
+                Console.Clear();
+                continue ;
+            }
+            
+            var parser = new Parser(line) ;
+            var tree = parser.Parse();
+            
+            // Revisa si se encontro algun bug durante la compilacion.
+            if(CompilatorTools.Bugs.Any())   
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed ;
+                Console.WriteLine(CompilatorTools.Bugs[0]);
+                Console.ForegroundColor = ConsoleColor.Green ;
+            }
+            
+            // Si no se encontro ninguno , se ejecuta el programa.
+            else    
+            {
+                for(int i = 0 ; i < tree.Roots.Length ; i++)
+                {
+                    var result = tree.Roots[i].Evaluate() ;
+                
+                    // Nuevamente revisa si encontro algun bug que no partio el programa.
+                    if(CompilatorTools.Bugs.Any())
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed ;
+                        Console.WriteLine(CompilatorTools.Bugs[0]);
+                
+                        Console.ForegroundColor = ConsoleColor.Green ;
+                        break ;
+                    }
+                    // Imprime el resultado del programa
+                    else
+                    {
+                        if(result != null)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue ;
+                            Console.WriteLine(result);
+                        }        
+                    }
+                }
+           }
+           crono.Stop();
+           Console.ForegroundColor = ConsoleColor.Yellow ;
+           Console.WriteLine("~ Execution time : {0} ms ~" , crono.ElapsedMilliseconds);
+        }
+    }
+}
+
+
---
+++ token.cs
@@ -0,0 +1,86 @@
+﻿namespace HULK ;
+
+/* Los tokens son unidades que tienen un significado especifico para el compilador
+
+Contienen las siguientes propiedades :
+    ._kind      :  Representa el tipo de token
+    ._position  :  Representa la posicion del token en la linea de codigo
+    ._text      :  Representa el "nombre" del token ("x" , ">=" , "function", etc)
+    ._value     :  Representa el valor del token(util en las variables)
+*/
+
+// Conjunto de Tokens y Expresiones del HULK
+public enum SyntaxKind
+{
+    // Operadores Aritmeticos
+    PlusSignToken ,
+    MinusToken ,
+    SlashToken ,
+    StarToken ,
+    ExponentToken ,
+    PercentageToken ,
+    EqualToken ,
+    ArrobaToken ,
+
+    // Operadores Logicos
+    NotToken ,
+    AndToken ,
+    OrToken ,
+    DobleAndToken ,
+    DobleOrToken ,
+    NotEqualToken ,
+    EqualEqualToken ,
+    MoreToken,
+    LessToken ,
+    MoreOrEqualToken ,
+    LessOrEqualToken ,
+    
+    // Tokens
+    LiteralToken , 
+    OpenParenthesisToken ,
+    CloseParenthesisToken ,
+    WhiteSpaceToken ,
+    CommaSeparatorToken,
+    ArrowToken,
+    BadToken ,
+    EOLToken ,
+    EOFToken ,
+
+
+    // Keywords & Identifier
+    LetKwToken,
+    IfKwToken ,
+    InKwToken ,
+    ThenKwToken,
+    ElseKwToken,
+    TrueToken,
+    FalseToken ,
+    IdentifierToken ,
+    FunctionKwToken,
+
+    // Expresiones
+    LiteralExpression ,
+    BinaryOperatorExpression ,
+    UnaryOperatorExpression ,
+    ParenthesizedExpression ,
+    VariableExpression,
+    DeclaredFunctionExpression ,
+    FunctionCallExpression ,
+    LetInExpression
+}
+
+public class SyntaxToken
+{
+    public SyntaxKind _kind ;
+    public int _position ;
+    public string _text  ;
+    public object _value ;
+
+    public SyntaxToken(SyntaxKind kind , int position , string text , object value)
+    {
+        _kind = kind ;
+        _position = position ;
+        _text = text ;
+        _value = value ;             
+    }
+}
